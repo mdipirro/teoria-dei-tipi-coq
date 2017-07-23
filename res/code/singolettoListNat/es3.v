891fd021cff13dcaba0fd092d5334e2ce075e4c6
Matteo Di Pirro <matteodipirro@gmail.com>
2017-07-23 12:22:11 +0200

    Risolto esercizio 3 con vettore

--- res/code/singolettoListNat/es3.v
+++ res/code/singolettoListNat/es3.v
@@ -2,82 +2,44 @@ From mathcomp Require Import ssreflect.
 Notation erefl := refl_equal.
 Open Scope list_scope.
 
-(*Fixpoint back {A} (l: lista_non_vuota A) : list A.
+Inductive vec (A : Type) : nat -> Type :=
+| Nil : vec A 0
+| Cons : forall n : nat, A -> vec A n -> vec A (S n).
+
+Arguments Nil {_}.
+Arguments Cons {_} {_} x xs.
+
+Definition non_vuota : vec nat 3 := (Cons 1 (Cons 2 (Cons 3 Nil))).
+
+Fixpoint back {A} {n: nat} (v: vec A (S n)) : vec A n.
 Proof.
-case l.
-move=> xs pf.
-case xs.
-  apply nil.
-move=> e es.
-case es.
-  apply (e :: nil).
-move=> y ys.
-apply (e :: (back (exist _ ys not_xnil_nil))).
+inversion v.
+destruct n.
+  apply X0.
+apply (Cons X (back _ _ X0)).
 Defined.
 
-Print back.*)
-
-Definition lista_non_vuota (A: Type) := { l: list A  | ~ l = nil }.
-
-Lemma ex_falso P : False -> P.
-Proof. move=> abs. case: abs. Qed.
-
-Lemma not_xnil_nil {A} (x: A) : ~(x :: nil = nil).
+Definition front {A} {n: nat} (v: vec A (S n)) : vec A n.
 Proof.
-move=> E.
-change (match x :: nil with nil => True | cons _ _ => False end).
-rewrite E.
-apply: I.
-Qed.
-
-Lemma not_xxs_nil {A} (x: A) (xs: list A) : ~ (x :: xs = nil).
-Proof.
-move=> E.
-change (match x :: xs with nil => True | cons _ _ => False end).
-rewrite E.
-apply: I.
-Qed.
-
-Definition front {A} (l: lista_non_vuota A) : list A.
-Proof.
-case: l.
-move=> xs _.
-case xs.
-  apply nil.
-move=> e es.
-apply: es.
+inversion v.
+apply X0.
 Defined.
 
-Definition last {A} (l: lista_non_vuota A) : A.
+Definition last {A} {n: nat} (v: vec A (S n)) : A.
 Proof.
-case: l.
-move=> xs.
-case: xs.
-  move=> abs.
-  apply: ex_falso.
-  apply: abs.
-  apply: erefl.
-move=> e es _.
-apply: e.
+inversion v.
+apply X.
 Defined.
 
-(*Fixpoint first {A} (l: lista_non_vuota A) : A.
+Fixpoint first {A} {n : nat} (v : vec A (S n)) : A.
 Proof.
-case: l.
-move=> xs.
-case: xs.
-  move=> abs.
-  apply: ex_falso.
-  apply: abs.
-  apply: erefl.
-move=> e es _.
-elim: es.
-  apply: e.
-Defined.*)
+inversion v.
+destruct n.
+  apply X.
+apply (first _ _ X0).
+Defined.
 
-
-Definition non_vuota : lista_non_vuota nat := 
-exist _ (1 :: 2 :: 3 :: 4 :: nil) (not_xxs_nil 1 (2 :: 3 :: 4 :: nil)).
-Eval compute in (front non_vuota).
-Eval compute in (last non_vuota).
-Eval compute in (first non_vuota).
+Eval compute in back non_vuota.
+Eval compute in front non_vuota.
+Eval compute in last non_vuota.
+Eval compute in first non_vuota.
