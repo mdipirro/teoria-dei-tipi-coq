1a314376bfc5becb0fdfc688ba83db4d114db25b
Matteo Di Pirro <matteodipirro@gmail.com>
2017-07-23 13:06:38 +0200

    Risolti gli esercizi sulla somma indiciata

---
+++ res/code/sommaIndiciata/es3.v
@@ -0,0 +1,43 @@
+From mathcomp Require Import ssreflect.
+Notation erefl := refl_equal.
+Open Scope list_scope.
+
+Lemma ex_falso P : False -> P.
+Proof. move=> abs. case: abs. Qed.
+
+(*Naturali positivi*)
+Definition nat_positivo := {n: nat | ~ n = 0}.
+Lemma not_Sn_zero (n: nat): ~((S n) = 0).
+Proof.
+case n.
+  by [].
+by [].
+Qed.
+
+Definition tre_positivo : nat_positivo := exist _ 3 (not_Sn_zero 2).
+(*
+Non corretta (giustamente):
+Definition zero_positivo: nat_positivo := exist _ 0 (not_Sn_zero 0).
+*)
+
+(*Lista non vuota*)
+Definition lista_non_vuota (A: Type) := { l: list A  | ~ l = nil }.
+
+Lemma not_xnil_nil {A} (x: A) : ~(x :: nil = nil).
+Proof.
+move=> E.
+change (match x :: nil with nil => True | cons _ _ => False end).
+rewrite E.
+apply: I.
+Qed.
+
+Lemma not_xxs_nil {A} (x: A) (xs: list A) : ~ (x :: xs = nil).
+Proof.
+move=> E.
+change (match x :: xs with nil => True | cons _ _ => False end).
+rewrite E.
+apply: I.
+Qed.
+
+Definition non_vuota : lista_non_vuota nat := 
+exist _ (1 :: 2 :: 3 :: 4 :: nil) (not_xxs_nil 1 (2 :: 3 :: 4 :: nil)).
