427f86c3415e08ddaeb9c5cc96c94fc1eca9ab3d
Matteo Di Pirro <matteodipirro@gmail.com>
2017-08-04 12:39:43 +0200

    Piccoli progressi

--- res/code/uguaglianzaProposizionale2/es2.v
+++ res/code/uguaglianzaProposizionale2/es2.v
@@ -1,65 +1,55 @@
 From mathcomp Require Import ssreflect.
+Require Import Coq.Logic.EqdepFacts.
 Notation erefl := refl_equal.
 
-Inductive N5 := zero | uno | due | tre | quattro.
+Inductive N3 := zero | one | two.
 
-Definition minore_di_5 := {x| x < 5}.
+Definition less_than_3 := {x| x < 3}.
 
 
 (* Trovo l'isomorfismo rispetto a {x| x<5} *)
-Definition f (n: minore_di_5) : N5 :=
+Definition lt3_to_N3 (n: less_than_3) : N3 :=
 match n with
   | exist 0 _ => zero
-  | exist 1 _ => uno
-  | exist 2 _ => due
-  | exist 3 _ => tre
-  | exist 4 _ => quattro
-  | exist _ _ => quattro
+  | exist (S m) _ => match m with 
+                    | 0 => one
+                    | S _ => two
+                   end
 end.
 
-(*Definition f (n: minore_di_5) : N5 :=
+Axiom l_0_3 : 0 < 3.
+Axiom l_1_3 : 1 < 3.
+Axiom l_2_3 : 2 < 3.
+Axiom l_SSSp_3 : forall p: nat, S (S (S p)) < 3.
+Axiom eq_l03 : forall h: 0 < 3, l_0_3 = h.
+Axiom eq_l13 : forall h: 1 < 3, l_1_3 = h.
+Axiom eq_l23 : forall h: 2 < 3, l_2_3 = h.
+(*Axiom eq_lSSSp3 : forall h: forall p: nat, S (S (S p)) < 3, l_2_3 = h.*)
+
+Definition N3_to_lt3 (n: N3) : less_than_3 :=
 match n with
-  exist 0 _ =>  match m with
-                  | 0 => zero
-                  | 1 => uno
-                  | 2 => due
-                  | 3 => tre
-                  | 4 => quattro
-                  | S (S (S (S (S _)))) => quattro
-                end
-end.*)
-
-Axiom l_0_5 : 0 < 5.
-Axiom l_1_5 : 1 < 5.
-Axiom l_2_5 : 2 < 5.
-Axiom l_3_5 : 3 < 5.
-Axiom l_4_5 : 4 < 5.
-
-Definition g (n: N5) : minore_di_5 :=
-match n with
-  | zero => exist _ 0 l_0_5
-  | uno => exist _ 1 l_1_5
-  | due => exist _ 2 l_2_5
-  | tre => exist _ 3 l_3_5
-  | quattro => exist _ 4 l_4_5
+  | zero => exist _ 0 l_0_3
+  | one => exist _ 1 l_1_3
+  | two => exist _ 2 l_2_3
 end.
 
-(*Definition pf1 (x: minore_di_5) : g (f x) = x :=
-match g (f x) with
-  | x => erefl
-end.
-
-Definition pf1 (x: minore_di_5) : eq x (g (f x)).
+Definition eq_lt3_lt3 (x: less_than_3) : eq x (N3_to_lt3 (lt3_to_N3 x)).
 Proof.
-case: x.
-move=> n p.
-simpl.
-case: x.
-move=> x p0.
+destruct x as [ n h ].
+destruct n as [ | [ | [ | p ]]]; simpl in *.
+replace h with l_0_3.
 apply: erefl.
-Defined.*)
+apply: eq_l03.
+replace h with l_1_3.
+apply: erefl.
+apply: eq_l13.
+replace h with l_2_3.
+apply: erefl.
+apply: eq_l23.
 
-Definition pf2 (n: N5) : f (g n) = n.
+Defined.
+
+Definition eq_n3_n3 (n: N3) : lt3_to_N3 (N3_to_lt3 n) = n.
 Proof.
 by case n.
 Defined.
