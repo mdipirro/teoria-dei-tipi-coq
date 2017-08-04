2c696400a60c8942c587ba4fcef24adb25932681
Matteo Di Pirro <matteodipirro@gmail.com>
2017-08-04 15:37:09 +0200

    Risolti (forse) gli esercizi 1 e 2 (in attesa di risposta dalla prof)

--- res/code/prop/es1.v
+++ res/code/prop/es1.v
@@ -24,6 +24,34 @@ Defined.
 Definition congiunzione (a b: Prop) : Prop := forall p: Prop, 
   implicazione (forall x: T a, forall y: T b, p) p.
 
+Definition congiunzione_el_a : forall g a b d : Prop,
+(implicazione (g /\ a) d) -> Prop.
+Proof.
+move=> g a b d hp.
+apply d.
+Defined.
+
+Definition congiunzione_el_b : forall g a b d : Prop,
+(implicazione (g /\ b) d) -> Prop.
+Proof.
+move=> g a b d hp.
+apply d.
+Defined.
+
 (* Disgiunzione *)
 Definition disgiunzione (a b: Prop) : Prop := forall p: Prop,
   implicazione (congiunzione (a -> p) (b -> p)) p.
+
+Definition disgiunzione_el_a : forall g a b: Prop,
+(implicazione g (a \/ b)) -> Prop.
+Proof.
+move=> g a b hp.
+apply a.
+Defined.
+
+Definition disgiunzione_el_b : forall g a b: Prop,
+(implicazione g (a \/ b)) -> Prop.
+Proof.
+move=> g a b hp.
+apply b.
+Defined.
--- res/code/prop/es2.v
+++ res/code/prop/es2.v
@@ -2,12 +2,18 @@ From mathcomp Require Import ssreflect.
 
 Definition T (a: Prop) := Type.
 
-Definition perogni {A} (c: A -> Prop) : Prop := forall x: A, c x.
-
 Definition implicazione (a b : Prop) : Prop := forall x: T a, b.
 
-Definition esiste {A} (a b: Prop) (c: A -> Prop) : Prop := forall p: Prop,
+(* Quantificatore esistenziale *)
+Definition esiste {A} (a: Prop) (c: A -> Prop) : Prop := forall p: Prop,
   implicazione (forall x: A, implicazione (c x) p) p.
 
+Definition esiste_el {A} : forall g a: Prop, forall C: A -> Prop,
+(implicazione g (esiste a C)) -> Prop.
+Proof.
+move=> g a C hp.
+apply a.
+Defined.
 
-
+(* Quantificatore universale *)
+(* Definito come primitivo in Coq*)
