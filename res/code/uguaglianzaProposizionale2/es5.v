f9742d5b5efead0f907e892df9870685c2c63b65
Matteo Di Pirro <matteodipirro@gmail.com>
2017-08-04 18:24:49 +0200

    Risolto esercizio 5

--- res/code/uguaglianzaProposizionale2/es5.v
+++ res/code/uguaglianzaProposizionale2/es5.v
@@ -10,6 +10,7 @@ Axiom el : forall (A : Type) (C : A -> A -> Type),
 Axiom el_refl : forall (A : Type) (C : A -> A -> Type)
     (CR : forall x : A, C x x),
     forall x : A, el A C CR x x (refl A x) = CR x.
+Axiom UIP : forall (A: Type) (x y: A) (p1 p2: eq A x y), p1 = p2. 
 
 End Leibniz.
 
@@ -53,46 +54,16 @@ Qed.
 
 Definition pf1 {A} (x y: A) (m: MartinLof.eq A x y) : eq m (g x y (f x y m)).
 Proof.
-apply: (MartinLof.el A (fun x y p => p = g x y (f x y p))) => x0.
+apply: (MartinLof.el A (fun x y p => p = g x y (f x y p))).
+move=> x0.
 by rewrite /f MartinLof.el_refl /g Leibniz.el_refl.
-Qed.
+Defined.
 
 
 Definition pf2 {A} (x y: A) (m: Leibniz.eq A x y) : eq m (f x y (g x y m)).
 Proof.
-Check (Leibniz.el A (fun x y => p = f x y (g x y p))).
-apply: (Leibniz.el A (fun a b => m = f x y (g x y m))).
-move=> x0.
-rewrite /g Leibniz.el_refl.
-Qed.
-
-(*
-
-Inductive MartinLof (A: Type) : A -> A -> Prop :=
-| ml_refl x : MartinLof A x x.
-
-Inductive Leibniz (A: Type) : A -> A -> Prop :=
-| l_refl x : Leibniz A x x.
-
-Definition f {A} {x y: A} (l: Leibniz A x y) : MartinLof A x y :=
-  match l with l_refl a => ml_refl A a end.
-
-Definition g {A} {x y : A} (m: MartinLof A x y) : Leibniz A x y :=
-  match m with ml_refl z => l_refl A z end.
-
-Lemma equiv_ml_l : forall A x y, MartinLof A x y <-> Leibniz A x y.
-Proof.
-move=> A x y.
-split.
-  move=> m.
-  apply (g m).
-move=> l.
-apply (f l).
-Qed.
-
-Definition pf1 {A} (x y : A) (l: Leibniz A x y) : g (f l) = l :=
-  match l with l_refl _ => eq_refl end.
-
-Definition pf2 {A} (x y : A) (m: MartinLof A x y) : f (g m) = m :=
-  match m with ml_refl _ => eq_refl end.
-*)
+move: (m).
+apply: (Leibniz.el A (fun x y => forall m, m = f x y (g x y m)) _ x y m).
+move=> z p.
+apply: (Leibniz.UIP A z z p (f z z (g z z p))).
+Defined.
