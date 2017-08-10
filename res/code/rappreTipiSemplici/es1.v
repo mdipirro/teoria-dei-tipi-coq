bf9dd6ec499af7bb76476dcc19a80721fcbfaeac
Matteo Di Pirro <matteodipirro@gmail.com>
2017-08-10 11:31:29 +0200

    Sistemato esercizio 2 e ottimizzato esercizio 1

--- res/code/rappreTipiSemplici/es1.v
+++ res/code/rappreTipiSemplici/es1.v
@@ -1,11 +1,11 @@
 From mathcomp Require Import ssreflect.
 Notation erefl := refl_equal.
 
-Lemma a_equal_b {A} (a: A) (b: A) : a = b.
+Lemma a_equal_b {A} (a b: A) : a = b.
 Proof.
 Admitted.
 
-Definition pf {A} (a: A) (b: A) : eq a b.
+Definition pf {A} (a b: A) : eq a b.
 Proof.
 apply a_equal_b.
 Defined. 
