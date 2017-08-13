2a7775efa5f3fb841a47bf4eec1b00b712ffd017
Matteo Di Pirro <matteodipirro@gmail.com>
2017-08-13 18:30:28 +0200

    Modificato esercizi 1 e 2

--- res/code/prop/es1.v
+++ res/code/prop/es1.v
@@ -6,52 +6,77 @@ Definition T (a: Prop) := Type.
 
 Definition implicazione (a b : Prop) : Prop := forall x: T a, b.
 
-Definition implicazione_el_a : forall a b g g' d: Prop,
-(implicazione ((g /\ (implicazione a b)) /\ g') d) -> Prop.
+Lemma implicazione_f (G A B : Prop) : 
+forall h: (forall c: G /\ A, B), forall c: G, A -> B.
 Proof.
-move=> a b g g' d hp.
-apply (implicazione g a).
-Defined.
+intros.
+apply (h (conj c H)).
+Qed.
 
-Definition implicazione_el_delta : forall a b g g' d: Prop,
-(implicazione ((g /\ (implicazione a b)) /\ g') d) -> Prop.
+Lemma implicazione_re (G G' A B D : Prop) : 
+forall h: ((forall c1: G, A) /\ (forall c2: B /\ G', D)),
+  forall c: G /\ (forall x: A, B) /\ G', D.
 Proof.
-move=> a b g g' d hp.
-apply (implicazione (b /\ g') d).
-Defined.
+intros.
+destruct h.
+destruct c.
+destruct H2.
+apply (H0 (conj (H2 (H H1)) H3)).
+Qed.
 
 (* Congiunzione *)
 Definition congiunzione (a b: Prop) : Prop := forall p: Prop, 
   implicazione (forall x: T a, forall y: T b, p) p.
 
-Definition congiunzione_el_a : forall g a b d : Prop,
-(implicazione (g /\ a) d) -> Prop.
+Lemma congiunzione_f (G A B : Prop) : 
+forall h: ((forall h1: G, A) /\ (forall h2: G, B)),
+  forall c: G, A /\ B.
 Proof.
-move=> g a b d hp.
-apply d.
-Defined.
+intros.
+destruct h.
+apply (conj (H c) (H0 c)).
+Qed.
 
-Definition congiunzione_el_b : forall g a b d : Prop,
-(implicazione (g /\ b) d) -> Prop.
+Lemma congiunzione_re1 (G A B D : Prop): forall h: (forall c: G /\ A, D),
+  forall c: G /\ (A /\ B), D.
 Proof.
-move=> g a b d hp.
-apply d.
-Defined.
+intros.
+apply (h (conj (proj1 c) ((proj1 (proj2 c))))).
+Qed.
+
+Lemma congiunzione_re2 (G A B D : Prop): forall h: (forall c: G /\ B, D),
+  forall c: G /\ (A /\ B), D.
+Proof.
+intros.
+apply (h (conj (proj1 c) ((proj2 (proj2 c))))).
+Qed.
 
 (* Disgiunzione *)
 Definition disgiunzione (a b: Prop) : Prop := forall p: Prop,
   implicazione (congiunzione (a -> p) (b -> p)) p.
 
-Definition disgiunzione_el_a : forall g a b: Prop,
-(implicazione g (a \/ b)) -> Prop.
+Lemma disgiunzione_f (G A B D : Prop) : 
+forall h: ((forall c1: G /\ A, D) /\ (forall c2: G /\ B, D)),
+  forall c: G /\ (A \/ B), D.
 Proof.
-move=> g a b hp.
-apply a.
-Defined.
+intros.
+destruct h.
+destruct c.
+destruct H2.
+  apply (H (conj H1 H2)).
+apply (H0 (conj H1 H2)).
+Qed.
 
-Definition disgiunzione_el_b : forall g a b: Prop,
-(implicazione g (a \/ b)) -> Prop.
+Lemma disgiunzione_re1 (G A B : Prop) : forall h: (forall c: G, A),
+  forall c: G, A \/ B.
 Proof.
-move=> g a b hp.
-apply b.
-Defined.
+intros.
+apply (or_introl (h c)).
+Qed.
+
+Lemma disgiunzione_re2 (G A B : Prop) : forall h: (forall c: G, B),
+  forall c: G, A \/ B.
+Proof.
+intros.
+apply (or_intror (h c)).
+Qed.
