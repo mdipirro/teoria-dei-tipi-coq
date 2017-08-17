cdf87f56bb2af862202f03645aa9138baff2d1b3
Matteo Di Pirro <matteodipirro@gmail.com>
2017-08-17 15:21:11 +0200

    Risolti esercizi 2, 3, 4 e 5 con le due uguaglianze

--- res/code/uguaglianzaProposizionale/es3.v
+++ res/code/uguaglianzaProposizionale/es3.v
@@ -6,9 +6,3 @@ Definition pf {A} {P} (x: A) (y: A) (z: P x) (w: eq x y) : eq y y.
 Proof.
 rewrite //=.
 Defined.
-
-Definition pf2 {A} (x: A) (y: A) (z: A) (w1: eq x y) (w2: eq y z) : eq x z.
-Proof.
-rewrite w1 w2.
-apply: erefl.
-Defined.
