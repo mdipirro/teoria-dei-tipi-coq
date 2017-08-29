f2f705f65a03d476f935bf8b8907cffd21c91e6b
Matteo Di Pirro <matteodipirro@gmail.com>
2017-08-29 17:03:13 +0200

    Rimosse definizioni da 9.4

--- res/code/traduzioneLogica/es4.v
+++ res/code/traduzioneLogica/es4.v
@@ -1,48 +1,3 @@
-From mathcomp Require Import ssreflect.
-Require Import Coq.Logic.FunctionalExtensionality.
-
-Inductive Boole :=
-  | inlb (a: unit)
-  | inrb (b: unit).
-
-Definition dis (z: eq (inlb tt) (inrb tt)) : unit.
-Proof.
-Admitted.
-
-Definition f {A} {B} (z: A + B) :
-  sigT (fun x: Boole => prod ((eq x (inrb tt)) -> A) (eq x (inlb tt) -> B)).
-Proof.
-case z.
-  move=> a.
-  exists (inrb tt).
-  rewrite //=.
-move=> b.
-  exists (inlb tt).
-  rewrite //=.
-Defined.
-
-Lemma eq_inla_inltt (a: unit) : eq (inlb a) (inlb tt).
-Proof.
-by case a.
-Qed.
-
-Lemma eq_inra_inrtt (a: unit) : eq (inrb a) (inrb tt).
-Proof.
-by case a.
-Qed.
-
-Definition g {A} {B} 
-  (w: sigT (fun x: Boole => prod ((eq x (inrb tt)) -> A) (eq x (inlb tt) -> B))) :
-  A + B.
-Proof.
-destruct w.
-destruct p.
-destruct x.
-apply (inr (b (eq_inla_inltt a0))).
-apply (inl (a (eq_inra_inrtt b0))).
-Defined.
-
-(*
 Definition pf1 {A} {B} (x: A + B): 
   eq x (g (f x)).
 Proof.
@@ -73,4 +28,3 @@ case: (f (g y)).
         rewrite (UIP_refl Boole (inlb tt) x).
         
 Admitted.
-*)
